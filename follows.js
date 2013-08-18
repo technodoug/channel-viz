bb07f0793fa50dedb06ff028ead458a2a0be561a
technodoug <technodoug42@gmail.com>
2013-08-17 20:28:58 -0400

    Update with basic feed data

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'LgrXnzyjOJbM9YSsjEgSiieswhWGSDhFzvhR1r3KfesZK2iR', // Unique master Xively API key to be used as a default
+		defaultFeeds	= '146707428', // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Swimming Pool Monitor', // Replaces Xively logo in the header
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
